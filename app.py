513f79c053f5abd8543574ff3c8a14ac3fe6d10d
LogicAtom <anthonykoz5909@gmail.com>
2021-10-02 12:45:01 +0000

    All CLI errors resolved

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+    Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -68,10 +67,10 @@ def login():
             # ensure hashed password matches user input
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(
-                            request.form.get("username")))
-                        return redirect(url_for(
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
                             "profile", username=session["user"]))
             else:
                 # invalid password match
