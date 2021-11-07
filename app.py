bef861cf2aa632a32a6e02c08f5d9fa386097831
LogicAtom <anthonykoz5909@gmail.com>
2021-11-07 04:29:50 +0000

    base.html, footer details added

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    """ def task_tasks is the default site url."""
+    """ def get_tasks is the default site url."""
     tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
